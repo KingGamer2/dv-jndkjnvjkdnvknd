e251b8dfde9c29571db902274064ecd886fbf3b3
KingGamer2 <40805903+KingGamer2@users.noreply.github.com>
2018-09-23 22:57:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,4 +2,6 @@ const https = require('https');
 const fs = require('fs');
 setInterval(function() {
     http.get("https://king-serv.herokuapp.com/");
-}, 300000)
+}, 300000).on('error', (e) => {
+  console.error(e);
+});
