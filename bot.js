1568d3d75691bbe7b844b541e76a3fa208c12b36
KingGamer2 <40805903+KingGamer2@users.noreply.github.com>
2018-09-23 23:25:20 +0300

    Delete bot.js

--- bot.js
+++
@@ -1,7 +0,0 @@
-const https = require('https');
-const fs = require('fs');
-setInterval(function() {
-    http.get("https://king-serv.herokuapp.com/");
-}, 300000).on('error', (e) => {
-  console.error(e);
-});
